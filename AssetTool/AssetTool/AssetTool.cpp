4dfe11ae073e24dacc79db8f5660daa0c1fb35b0
LeeJJ07 <jkj9513@naver.com>
2024-10-10 17:56:54 +0900

    feat: 스프라이트 툴 저장 기능 빼고 완료

--- AssetTool/AssetTool/AssetTool.cpp
+++ AssetTool/AssetTool/AssetTool.cpp
@@ -1,11 +1,16 @@
 ﻿
 // AssetTool.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
+#define _CRT_SECURE_NO_WARNINGS 
 
 #include "framework.h"
 #include "AssetTool.h"
 #include <fstream>
 #include <commdlg.h>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 #define MAX_LOADSTRING 100
 
@@ -21,12 +26,30 @@ HDC g_hTextImgDC;
 HBITMAP g_hTextImgBMP;
 int g_TextImgWidth;
 int g_TextImgHeight;
+int g_StartImgX;
+int g_StartImgY;
+
 
 bool g_bNowDrow = false;
 POINT g_MarginPos = { 0, 0 };
 POINT g_CharPos = { 0, 0 };
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+int startCenterX, startCenterY;
+int width, height, length, spacingX;
+int R, G, B;
+vector<POINT> pos;
+
+HWND hDlg = NULL;
+
+COLORREF GetPixelColorAtMouseClick();
+void SaveCroppedBitmap(HDC hdc, HWND hWnd, const TCHAR* filename);
+void SaveBitmapToFile(HDC hdc, HBITMAP hBitmap, int width, int height, const TCHAR* filename);
+void DrawArea(HDC hdc);
+void GameInit(TCHAR filename[]);
+void DrawBitmapDoubleBuffering(HWND hWnd, HDC hdc);
+void SetDlgItemTextFromAnsi(HWND hDlg, int nIDDlgItem, const std::string& str);
+void GameEnd();
+
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -56,10 +79,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
-    // 기본 메시지 루프입니다:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (hDlg == NULL || !IsDialogMessage(hDlg, &msg))  // 다이얼로그 메시지 처리
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
@@ -69,13 +91,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     return (int) msg.wParam;
 }
 
-
-
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -97,16 +112,6 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
@@ -123,6 +128,60 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    UpdateWindow(hWnd);
 
    return TRUE;
+}
+COLORREF GetPixelColorAtMouseClick()
+{
+    // 마우스 커서 위치 가져오기
+    POINT mousePos;
+    if (GetCursorPos(&mousePos))
+    {
+        // 화면의 DC(디바이스 컨텍스트) 가져오기
+        HDC hdcScreen = GetDC(NULL);
+        if (hdcScreen)
+        {
+            // 픽셀 색상 가져오기
+            COLORREF color = GetPixel(hdcScreen, mousePos.x, mousePos.y);
+            // DC 해제
+            ReleaseDC(NULL, hdcScreen);
+            return color;
+        }
+    }
+    return RGB(0, 0, 0); // 오류 발생 시 기본값
+}
+
+void DrawArea(HDC hdc)
+{
+    if (!width || !height || !length) 
+        return;
+
+    startCenterX = (rectView.right - (length - 1) * (width + spacingX)) / 2;
+    startCenterY = rectView.bottom / 2;
+
+    // 빨간색 테두리 설정
+    HPEN hPen = CreatePen(PS_SOLID, 1, RGB(255, 0, 0));  // 빨간색 테두리 펜 생성
+    HBRUSH hBrush = (HBRUSH)GetStockObject(NULL_BRUSH);  // 투명한 내부를 위한 브러시
+
+    HPEN hOldPen = (HPEN)SelectObject(hdc, hPen);  // 기존 펜 저장 및 새 펜 선택
+    HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hBrush);  // 기존 브러시 저장 및 새 브러시 선택
+
+    for (int idx = 0; idx < length; idx++)
+    {
+        int left = startCenterX + (width + spacingX) * idx - width / 2;
+        int right = startCenterX + (width + spacingX) * idx + width / 2;
+        int top = startCenterY - height / 2;
+        int bottom = startCenterY + height / 2;
+
+        // 사각형 그리기
+        Rectangle(hdc, left, top, right, bottom);
+    }
+
+    // 원래 펜과 브러시 복원
+    SelectObject(hdc, hOldPen);
+    SelectObject(hdc, hOldBrush);
+
+    // 리소스 해제
+    DeleteObject(hPen);
+    DeleteObject(hBrush);
 }
 
 void GameInit(TCHAR filename[])
@@ -170,8 +229,43 @@ void DrawBitmapDoubleBuffering(HWND hWnd, HDC hdc)
     // 비트맵을 더블 버퍼에 그리기
     if (g_hTextImgDC != NULL)
     {
-        BitBlt(hDoubleBufferDC, g_CharPos.x, g_CharPos.y, g_TextImgWidth, g_TextImgHeight, g_hTextImgDC, 0, 0, SRCCOPY);
+        BitBlt(hDoubleBufferDC, g_CharPos.x, g_CharPos.y, g_TextImgWidth, g_TextImgHeight, g_hTextImgDC, g_StartImgX, g_StartImgY, SRCCOPY);
     }
+    if (pos.size())
+    {
+        // 텍스트 색상 및 브러시 설정
+        SetBkMode(hDoubleBufferDC, TRANSPARENT);
+        SetTextColor(hDoubleBufferDC, RGB(255, 255, 255)); // 흰색 텍스트
+        HBRUSH hBrush = (HBRUSH)GetStockObject(WHITE_BRUSH);
+        HPEN hPen = CreatePen(PS_SOLID, 1, RGB(255, 255, 255)); // 흰색 펜
+        SelectObject(hDoubleBufferDC, hPen);
+
+        int offsetY = 0; // Y축 오프셋 초기화
+
+        for (const POINT& p : pos)
+        {
+            // 점 찍기
+            SetPixel(hDoubleBufferDC, p.x, p.y, RGB(255, 255, 255)); // 흰색 점
+
+            // 좌표 텍스트 생성
+            std::string text = "(" + std::to_string(p.x) + ", " + std::to_string(p.y) + ")";
+
+            // 텍스트 위치를 오른쪽 위에 맞추기 위해 좌표 조정
+            int textWidth = text.length() * 8; // 대략적인 텍스트 폭 계산
+            int textX = rectView.right - textWidth - 5; // 오른쪽 여백 설정
+            int textY = 5 + offsetY; // 위쪽 여백 설정
+
+            // 텍스트 출력
+            TextOutA(hDoubleBufferDC, textX, textY, text.c_str(), text.length());  // 오른쪽 위에 출력
+            offsetY += 20; // 다음 텍스트의 Y축 위치를 아래로 이동
+        }
+
+        DeleteObject(hPen);  // 펜 자원 해제
+    }
+
+
+    //사각형 그리기
+    DrawArea(hDoubleBufferDC);
 
     // 더블 버퍼의 내용을 실제 화면에 복사
     BitBlt(hdc, 0, 0, rectView.right, rectView.bottom, hDoubleBufferDC, 0, 0, SRCCOPY);
@@ -194,34 +288,143 @@ void GameEnd()
     }
 }
 
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
+void SaveBitmapToFile(HBITMAP hBitmap, const TCHAR* filePath)
+{
+    BITMAP bmp;
+    GetObject(hBitmap, sizeof(BITMAP), &bmp); // 비트맵 정보 가져오기
+
+    // BMP 파일 헤더 크기
+    DWORD bmpFileHeaderSize = sizeof(BITMAPFILEHEADER);
+    DWORD bmpInfoHeaderSize = sizeof(BITMAPINFOHEADER);
+    DWORD bmpSize = bmp.bmWidthBytes * bmp.bmHeight; // 비트맵 데이터 크기
+
+    // BMP 파일 헤더
+    BITMAPFILEHEADER bfh;
+    bfh.bfType = 0x4D42; // 'BM'
+    bfh.bfSize = bmpFileHeaderSize + bmpInfoHeaderSize + bmpSize; // 전체 파일 크기
+    bfh.bfReserved1 = 0;
+    bfh.bfReserved2 = 0;
+    bfh.bfOffBits = bmpFileHeaderSize + bmpInfoHeaderSize; // 비트맵 데이터 시작 위치
+
+    // BMP 정보 헤더
+    BITMAPINFOHEADER bih;
+    bih.biSize = bmpInfoHeaderSize;
+    bih.biWidth = bmp.bmWidth;
+    bih.biHeight = bmp.bmHeight;
+    bih.biPlanes = 1;
+    bih.biBitCount = 32; // RGBA 형식
+    bih.biCompression = BI_RGB;
+    bih.biSizeImage = 0; // 비트맵 데이터 크기 (0으로 설정하면 크기를 자동으로 계산)
+    bih.biXPelsPerMeter = 0;
+    bih.biYPelsPerMeter = 0;
+    bih.biClrUsed = 0;
+    bih.biClrImportant = 0;
+
+    // 파일 열기
+    HANDLE hFile = CreateFile(filePath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        MessageBox(NULL, L"파일을 열 수 없습니다.", L"오류", MB_OK);
+        return;
+    }
+
+    DWORD written;
+
+    // BMP 파일 헤더 쓰기
+    WriteFile(hFile, &bfh, bmpFileHeaderSize, &written, NULL);
+    // BMP 정보 헤더 쓰기
+    WriteFile(hFile, &bih, bmpInfoHeaderSize, &written, NULL);
+
+    // 비트맵 데이터 가져오기
+    HDC hdc = CreateCompatibleDC(NULL);
+    HBITMAP hOldBitmap = (HBITMAP)SelectObject(hdc, hBitmap);
+
+    // 비트맵 데이터를 파일에 쓰기
+    BYTE* pBits = new BYTE[bmpSize];
+    GetDIBits(hdc, hBitmap, 0, bmp.bmHeight, pBits, (BITMAPINFO*)&bih, DIB_RGB_COLORS);
+
+    // 비트맵 데이터는 32비트로 저장하므로 패딩을 고려해야 합니다.
+    for (int i = 0; i < bmp.bmHeight; i++)
+    {
+        WriteFile(hFile, pBits + (bmp.bmHeight - 1 - i) * bmp.bmWidthBytes, bmp.bmWidthBytes, &written, NULL);
+    }
+
+    // 자원 해제
+    delete[] pBits;
+    SelectObject(hdc, hOldBitmap);
+    DeleteDC(hdc);
+    CloseHandle(hFile);
+}
 
 BOOL CALLBACK Dialog1_Proc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    static HWND targetHwnd = FindWindow(NULL, L"AssetTool");
     switch (uMsg)
     {
     case WM_INITDIALOG:
-        return TRUE; // 초기화 완료
+    {
+        string strR = to_string(R);
+        SetDlgItemTextFromAnsi(hDlg, IDC_EDIT_readR, strR.c_str());
 
+        // G 값을 에디트 컨트롤에 설정
+        string strG = to_string(G);
+        SetDlgItemTextFromAnsi(hDlg, IDC_EDIT_readG, strG.c_str());
+
+        // B 값을 에디트 컨트롤에 설정
+        string strB = to_string(B);
+        SetDlgItemTextFromAnsi(hDlg, IDC_EDIT_readB, strB.c_str());
+    }
+        return TRUE; // 초기화 완료
     case WM_COMMAND:
     {
         int wmId = LOWORD(wParam);
         switch (wmId)
         {
-            // 다른 명령 처리 가능
+        case IDC_BUTTON_check:
+        {
+            TCHAR buffer[64];  
+
+            GetDlgItemText(hDlg, IDC_EDIT_width, buffer, 64);
+            width = _ttoi(buffer); 
+            GetDlgItemText(hDlg, IDC_EDIT_height, buffer, 64);
+            height = _ttoi(buffer);
+            GetDlgItemText(hDlg, IDC_EDIT_length, buffer, 64);
+            length = _ttoi(buffer);  
+            GetDlgItemText(hDlg, IDC_EDIT_spacingX, buffer, 64);
+            spacingX = _ttoi(buffer); 
+
+            if (targetHwnd)
+                InvalidateRect(targetHwnd, NULL, TRUE);
+        }
+            break;
+        case IDC_BUTTON_cut:
+        {
+            g_TextImgWidth = width * length + spacingX * (length - 1);
+            g_TextImgHeight = height;
+
+            g_StartImgX = rectView.right / 2 - g_CharPos.x - g_TextImgWidth / 2;
+            g_StartImgY = rectView.bottom / 2 - g_CharPos.y - height / 2;
+
+            g_CharPos.x = rectView.right / 2 - g_TextImgWidth / 2;
+            g_CharPos.y = rectView.bottom / 2 - g_TextImgHeight / 2;
+
+            if (targetHwnd)
+                InvalidateRect(targetHwnd, NULL, TRUE);
+        }   
+            break;
+        case IDC_BUTTON_pickPosAgain:
+            pos.clear();
+            if (targetHwnd)
+                InvalidateRect(targetHwnd, NULL, TRUE);
+            break;
+        case IDC_BUTTON_save:
+        {
+            SaveBitmapToFile(g_hTextImgBMP, L"output.bmp");
+        }
+            break;
         }
     }
     break;
-
     case WM_CLOSE:  // 'X' 버튼을 눌렀을 때 처리
         EndDialog(hDlg, 0);  // 다이얼로그를 종료하고 0을 반환
         return TRUE;
@@ -235,6 +438,9 @@ BOOL CALLBACK Dialog1_Proc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 #define MAX_FILENAME_SIZE 100   
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    static int sx = 0;
+    static int sy = 0;
+
     OPENFILENAME Ofn;
     TCHAR sFilePathName[MAX_FILENAME_SIZE] = _T("");
     static TCHAR sFilter[] = L"모든 파일\0*.*\0텍스트 파일\0*.txt\0비트맵 파일\0*.bmp";
@@ -268,9 +474,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     GameInit(sFilePathName);
                     InvalidateRect(hWnd, NULL, FALSE);
                 }
+
+                g_StartImgX = 0;
+                g_StartImgY = 0;
+                g_CharPos.x = rectView.right / 2 - g_TextImgWidth / 2;
+                g_CharPos.y = rectView.bottom / 2 - g_TextImgHeight / 2;
+
+                pos.clear();
+
                 break;
             case ID_MENU_Set:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_Setup), hWnd, Dialog1_Proc);
+                hDlg = CreateDialog(hInst, MAKEINTRESOURCE(IDD_Setup), hWnd, Dialog1_Proc);
+                ShowWindow(hDlg, SW_SHOW);
                 break;
             case IDM_EXIT:
                 DestroyWindow(hWnd);
@@ -282,8 +497,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     case WM_LBUTTONDOWN:
     {
-        static int sx = 0;
-        static int sy = 0;
         sx = LOWORD(lParam);
         sy = HIWORD(lParam);
 
@@ -298,6 +511,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
     }
     break;
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        case VK_LEFT:
+            g_CharPos.x--;
+            break;
+        case VK_RIGHT:
+            g_CharPos.x++;
+            break;
+        case VK_UP:
+            g_CharPos.y--;
+            break;
+        case VK_DOWN:
+            g_CharPos.y++;
+            break;
+        }
+        InvalidateRect(hWnd, NULL, FALSE);
+        break;
 
     case WM_MOUSEMOVE:
     {
@@ -312,6 +543,43 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         g_bNowDrow = false;
     }
     break;
+    case WM_RBUTTONDOWN:
+    {
+        sx = LOWORD(lParam);
+        sy = HIWORD(lParam);
+        
+        sx = g_TextImgWidth / 2 - (rectView.right / 2 - sx);
+        sy = g_TextImgHeight / 2 - rectView.bottom / 2 + sy;
+        if (pos.size() < length)
+        {
+            pos.push_back({ sx, sy });
+            InvalidateRect(hWnd, NULL, FALSE);
+        }
+    }
+        break;
+    case WM_MOUSEWHEEL:
+        if ((SHORT)HIWORD(wParam) > 0) //마우스휠을 올릴 경우
+        {
+            COLORREF color = GetPixelColorAtMouseClick();
+            R = GetRValue(color);
+            G = GetGValue(color);
+            B = GetBValue(color);
+
+            if (hDlg != NULL)
+            {
+                string strR = to_string(R);
+                SetDlgItemTextFromAnsi(hDlg, IDC_EDIT_readR, strR.c_str());
+
+                // G 값을 에디트 컨트롤에 설정
+                string strG = to_string(G);
+                SetDlgItemTextFromAnsi(hDlg, IDC_EDIT_readG, strG.c_str());
+
+                // B 값을 에디트 컨트롤에 설정
+                string strB = to_string(B);
+                SetDlgItemTextFromAnsi(hDlg, IDC_EDIT_readB, strB.c_str());
+            }
+        }
+        break;
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
@@ -330,4 +598,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
+}
+void SetDlgItemTextFromAnsi(HWND hDlg, int nIDDlgItem, const std::string& str)
+{
+    wchar_t wBuffer[256];  // 유니코드 문자열 버퍼
+    MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, wBuffer, 256);  // ANSI -> 유니코드 변환
+    SetDlgItemText(hDlg, nIDDlgItem, wBuffer);  // 변환된 유니코드 문자열로 텍스트 설정
 }
--- AssetTool/AssetTool/resource.h
+++ AssetTool/AssetTool/resource.h
@@ -5,7 +5,7 @@
 #define IDC_MYICON                      2
 #define IDD_ASSETTOOL_DIALOG            102
 #define IDS_APP_TITLE                   103
-#define IDM_ABOUT						104
+#define IDM_ABOUT                       104
 #define IDM_EXIT                        105
 #define IDI_ASSETTOOL                   107
 #define IDI_SMALL                       108
@@ -16,8 +16,13 @@
 #define IDC_EDIT_height                 1001
 #define IDC_EDIT_length                 1002
 #define IDC_BUTTON_cut                  1003
-#define IDC_BUTTON_fix                  1005
+#define IDC_EDIT_spacingX               1004
+#define IDC_BUTTON_pickPosAgain         1005
 #define IDC_BUTTON_save                 1006
+#define IDC_BUTTON_check                1007
+#define IDC_EDIT_readR                  1010
+#define IDC_EDIT_readG                  1011
+#define IDC_EDIT_readB                  1012
 #define ID_OpenFile                     2000
 #define ID_FileSave                     2001
 #define ID_MENU_Set                     2002
