c84ba11aa4e0ab26649b93052347f876010bb61a
LeeJJ07 <jkj9513@naver.com>
2024-10-22 16:27:25 +0900

    feat: 이미지 데이터 몬스터 & 플레이어 저장 완료

--- Kirby_SupernovaVanguard/Kirby_SupernovaVanguard/framework.h
+++ Kirby_SupernovaVanguard/Kirby_SupernovaVanguard/framework.h
@@ -31,10 +31,15 @@
 
 static enum ECharacterType { KIRBY = 1, DDD, METANIHGT, MABOROA };
 static enum ECharacterState { IDLE, WALK, ATTACK };
+
+//ObjectImage enum 데이터와 txt 파일의 데이터 순서가 일치해야함.
 static enum ObjectImage
 {
 	kirby_Idle,		 kirby_Walk,		kirby_Attack,		 ddd_Idle,			ddd_Walk,			 ddd_Attack,
-	meta_Idle,		 meta_Walk,			meta_Attack,		 maboroa_Idle,		maboroa_Walk,		 maboroa_Attack
+	meta_Idle,		 meta_Walk,			meta_Attack,		 maboroa_Idle,		maboroa_Walk,		 maboroa_Attack,
+	runner_Walk,	 spear_Walk,		spaer_Attack,		 wingbug_Walk,		fireman_Walk,		 fireman_Attack,
+	landmine_Idle,	 KFM_Attack,		gaogao_Attack,		 boss_eye,			boss_mode1,			 boss_mode2,
+	boss_Attack1,	 boss_Attack2,		boss_Attack3
 };
 enum OBJECTINDEX
 {
